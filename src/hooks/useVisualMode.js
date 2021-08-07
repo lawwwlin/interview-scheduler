ae8454e19cbdbda85d0de1e97bba20b6319611e7
Lawrence Lin <lawwwlin@gmail.com>
2021-08-06 17:54:49 -0700

    install react-testing-library to test hooks, and add custom hooks

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      history.pop();
+    }
+    setMode(newMode);
+    history.push(newMode);
+    setHistory([...history]);
+  };
+
+  function back() {
+    if (history[history.length - 1] !== initial) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  };
+  
+  return { mode, transition, back };
+}
+
