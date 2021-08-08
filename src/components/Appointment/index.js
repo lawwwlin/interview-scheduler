160fda3f62e11ef56b6b46c4204dbbd00c1df60b
Lawrence Lin <lawwwlin@gmail.com>
2021-08-07 21:30:48 -0700

    fix bug where edit form is passing in interviewer object instead of interviewr id

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -71,7 +71,7 @@ export default function Appointment(props) {
       )}
       {mode === EDIT && <Form 
         name={props.interview.student}
-        interviewer={props.interview.interviewer}
+        interviewer={props.interview.interviewer.id}
         interviewers={props.interviewers}
         onSave={(name, interviewer) => {save(name, interviewer)}}
         onCancel={() => {transition(SHOW)}}
--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -10,8 +10,8 @@ export default function InterviewerList(props) {
       name={interviewer.name}
       avatar={interviewer.avatar}
       key={interviewer.id}
-      selected={interviewer.id === props.value.id}
-      setInterviewer={(event) => props.onChange(interviewer)}
+      selected={interviewer.id === props.value}
+      setInterviewer={(event) => props.onChange(interviewer.id)}
     />
   });
 
